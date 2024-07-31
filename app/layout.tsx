0743743e65330723a747a23e83322b842cbb0eef
wlkmkyq <130369148+wlkmkyq@users.noreply.github.com>
2024-07-31 14:25:47 +0800

    Update layout.tsx
    修改标题

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,8 +10,8 @@ import { GoogleTagManager } from "@next/third-parties/google";
 const serverConfig = getServerSideConfig();
 
 export const metadata: Metadata = {
-  title: "NextChat",
-  description: "Your personal ChatGPT Chat Bot.",
+  title: "梦链Studio",
+  description: "梦链剧本开发后台 2024年 JHFkk",
   viewport: {
     width: "device-width",
     initialScale: 1,
