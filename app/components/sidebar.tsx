92121a8068f706b120ee75f9c0c5580624d37d7a
wlkmkyq <130369148+wlkmkyq@users.noreply.github.com>
2024-07-31 14:32:18 +0800

    Update sidebar.tsx
    修改网站标题

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -155,10 +155,10 @@ export function SideBar(props: { className?: string }) {
     >
       <div className={styles["sidebar-header"]} data-tauri-drag-region>
         <div className={styles["sidebar-title"]} data-tauri-drag-region>
-          NextChat
+          梦链 Studio
         </div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          梦链开发后台 2024 JHFkk
         </div>
         <div className={styles["sidebar-logo"] + " no-dark"}>
           <ChatGptIcon />
