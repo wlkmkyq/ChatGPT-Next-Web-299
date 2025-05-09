fbd4bd8a13c8e0a29629fa86884bbf14ff695047
wlkmkyq <130369148+wlkmkyq@users.noreply.github.com>
2025-05-09 16:18:57 +0800

    Update chat.ts
    fix 4000 to 8000

--- app/store/chat.ts
+++ app/store/chat.ts
@@ -597,7 +597,7 @@ export const useChatStore = createPersistStore(
 
         const historyMsgLength = countMessages(toBeSummarizedMsgs);
 
-        if (historyMsgLength > modelConfig?.max_tokens ?? 4000) {
+        if (historyMsgLength > modelConfig?.max_tokens ?? 8000) {
           const n = toBeSummarizedMsgs.length;
           toBeSummarizedMsgs = toBeSummarizedMsgs.slice(
             Math.max(0, n - modelConfig.historyMessageCount),
