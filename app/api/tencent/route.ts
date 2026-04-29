cfd3033f2cef82be8de7762667d3927d0c3201ec
jhfkk <820741@qq.com>
2026-04-29 20:22:27 +0800

    Fix tencent

--- app/api/tencent/route.ts
+++ app/api/tencent/route.ts
@@ -1,20 +1,12 @@
 import { ApiPath } from "@/app/constant";
 import { NextRequest } from "next/server";
-import { handle as openaiHandler } from "../../openai";
-import { handle as proxyHandler } from "../../proxy";
+
 
 async function handle(
   req: NextRequest,
   { params }: { params: { provider: string; path: string[] } },
 ) {
-  const apiPath = `/api/${params.provider}`;
-  console.log(`[${params.provider} Route] params `, params);
-  switch (apiPath) {
-    case ApiPath.OpenAI:
-      return openaiHandler(req, { params });
-    default:
-      return proxyHandler(req, { params });
-  }
+
 }
 
 export const GET = handle;
