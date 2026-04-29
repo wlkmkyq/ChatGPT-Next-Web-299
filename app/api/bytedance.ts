3a2c10cfd7bb14ece9e256cd015a6914160a217f
jhfkk <820741@qq.com>
2026-04-29 20:40:28 +0800

    Fix route.ts

--- app/api/bytedance.ts
+++ app/api/bytedance.ts
@@ -1,10 +1,10 @@
 import { NextRequest, NextResponse } from "next/server";
-import { handle as openaiHandler } from "openai";
+
 
 export async function handle(
   req: NextRequest,
   { params }: { params: { path: string[] } },
 ) 
 {
-	return openaiHandler(req, { params });
+
 }
